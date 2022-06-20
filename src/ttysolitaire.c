986d0486ea74660ab8539355b8a12e57fe3460c5
aclist <92275929+aclist@users.noreply.github.com>
2022-06-20 23:01:17 +0900

    Force default background

--- src/ttysolitaire.c
+++ src/ttysolitaire.c
@@ -1,143 +1,157 @@
+#include <errno.h>
+#include <getopt.h>
+#include <libgen.h>
+#include <locale.h>
 #include <ncurses.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#include "card.h"
-#include "deck.h"
+#include "common.h"
 #include "game.h"
-#include "gui.h"
+#include "keyboard.h"
 
-static const char *card_suits[4] = {"\u2666", "\u2660", "\u2665", "\u2663"};
-static const char *card_values[13] = {"  A ", "  2 ", "  3 ",  "  4 ", "  5 ", "  6 ", "  7 ",
-                                      "  8 ", "  9 ", "  10 ", "  J ", "  Q ", "  K "};
+#ifndef VERSION
+#define VERSION "n/a"
+#endif
 
-static void draw_value(struct card *card) {
-  mvwprintw(card->frame->window, 0, 0, "%s", card_values[card->value]);
-//  mvwprintw(card->frame->window, 4, 7 - strlen(card_values[card->value]),
- //           "%s", card_values[card->value]);
-}
+const char *program_name;
+struct game game;
 
-static void draw_suit(struct card *card) {
-  if (game.four_color_deck == 0) {
-    if (card->suit % 2 == 0) {
-      wattron(card->frame->window, COLOR_PAIR(RED_ON_WHITE));
-    } else {
-      wattron(card->frame->window, COLOR_PAIR(BLACK_ON_WHITE));
-    }
-  } else {
-    switch (card->suit) {
-    case SPADES:
-      wattron(card->frame->window, COLOR_PAIR(GREEN_ON_WHITE));
+void version();
+void usage(const char *);
+void draw_greeting();
+
+int main(int argc, char *argv[]) {
+  int option;
+  int option_index;
+  int passes_through_deck = 3;
+  static int four_color_deck;
+  static int no_background_color;
+  static const struct option options[] = {
+      {"help", no_argument, NULL, 'h'},
+      {"version", no_argument, NULL, 'v'},
+      {"passes", required_argument, NULL, 'p'},
+      {"four-color-deck", no_argument, &four_color_deck, 1},
+      {"b", no_argument, &no_background_color, 1},
+      {0, 0, 0, 0}};
+
+  program_name = basename(argv[0]);
+
+  while ((option = getopt_long(argc, argv, "hvp:", options, &option_index)) !=
+         -1) {
+    switch (option) {
+    case 'v':
+      version();
+      exit(0);
+    case 'p':
+      passes_through_deck = atoi(optarg);
       break;
-    case DIAMONDS:
-      wattron(card->frame->window, COLOR_PAIR(YELLOW_ON_WHITE));
-      break;
-    case CLUBS:
-      wattron(card->frame->window, COLOR_PAIR(BLACK_ON_WHITE));
-      break;
-    case HEARTS:
+    case 'h':
+    case '?':
+      usage(program_name);
+      exit(0);
+    case 0:
+      /* If this option set a "no_argument" flag, do nothing else now. */
+      printf("options[option_index].name: %s\n", options[option_index].name);
+      if (options[option_index].flag != 0)
+        break;
     default:
-      wattron(card->frame->window, COLOR_PAIR(RED_ON_WHITE));
-      break;
+      usage(program_name);
+      exit(0);
     }
   }
-  mvwprintw(card->frame->window, 0, strlen(card_values[card->value]),
-            "%s", card_suits[card->suit]);
-//  mvwprintw(card->frame->window, 4, 6 - strlen(card_values[card->value]),
-//            "%s", card_suits[card->suit]);
-  if (card->suit % 2 == 0) {
-    wattroff(card->frame->window, COLOR_PAIR(RED_ON_WHITE));
-    wbkgd(card->frame->window, COLOR_PAIR(RED_ON_WHITE));
+
+  setlocale(LC_ALL, "");
+  initscr();
+  raw();
+  noecho();
+  keypad(stdscr, TRUE);
+  start_color();
+  init_color(COLOR_GREEN,150, 200, 250);
+  init_color(COLOR_YELLOW,190,255,255);
+  curs_set(FALSE);
+  set_escdelay(0);
+  if (no_background_color) {
+    use_default_colors();
   } else {
-    wattroff(card->frame->window, COLOR_PAIR(BLACK_ON_WHITE));
+    use_default_colors();
+    //assume_default_colors(COLOR_WHITE, COLOR_BLACK);
   }
-}
+  init_pair(1, COLOR_WHITE, COLOR_YELLOW);
+  init_pair(2, COLOR_RED, COLOR_YELLOW);
+  init_pair(3, COLOR_GREEN, COLOR_WHITE);
+  init_pair(4, COLOR_YELLOW, COLOR_WHITE);
+  init_pair(5, COLOR_WHITE, COLOR_GREEN);
+  init_pair(6, COLOR_WHITE, COLOR_GREEN);
 
-static void draw_front(struct card *card) {
-  wbkgd(card->frame->window, COLOR_PAIR(BLACK_ON_WHITE));
-  wborder(card->frame->window, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-  draw_value(card);
-  draw_suit(card);
-}
+  int key;
 
-static void draw_back(struct card *card) {
-  wbkgd(card->frame->window, COLOR_PAIR(WHITE_ON_BLUE));
-  box(card->frame->window, 0, 0);
-}
-
-void draw_card(struct card *card) {
-  if (card->face == EXPOSED) {
-    draw_front(card);
-  } else {
-    draw_back(card);
-  }
-  wrefresh(card->frame->window);
-}
-
-void draw_stack(struct stack *stack) {
-  if (stack_empty(stack)) {
-    box(stack->card->frame->window, 0, 0);
-    if (stock_stack(stack)) {
-      if (game.passes_through_deck_left >= 1) {
-        mvwprintw(stack->card->frame->window, 2, 3, "O");
-      } else {
-        mvwprintw(stack->card->frame->window, 2, 3, "X");
-      }
-    }
-    wrefresh(stack->card->frame->window);
-  } else {
-    if (maneuvre_stack(stack)) {
-      struct stack *stack_reversed_stack = stack_reverse(stack);
-      for (struct stack *i = stack_reversed_stack; i; i = i->next) {
-        draw_card(i->card);
-      }
-      stack_free(stack_reversed_stack);
-    } else {
-      draw_card(stack->card);
+  while (!term_size_ok()) {
+    clear();
+    mvprintw(1, 1, SMALL_TERM_MSG);
+    refresh();
+    if ((key = getch()) == 'q' || key == 'Q') {
+      endwin();
+      return (0);
     }
   }
+
+  clear();
+  //draw_greeting();
+  refresh();
+  game_init(&game, passes_through_deck, four_color_deck);
+
+ // for (;;) {
+ //   if ((key = getch()) == 'q' || key == 'Q') {
+ //     endwin();
+ //     return (0);
+ //   }
+ //   if (term_size_ok()) {
+ //     clear();
+ //     draw_greeting();
+ //     refresh();
+ //     if (key == KEY_SPACEBAR) {
+ //       clear();
+ //       refresh();
+ //       game_init(&game, passes_through_deck, four_color_deck);
+ //       break;
+ //     }
+ //   } else if (key == KEY_RESIZE) {
+ //     clear();
+ //     mvprintw(1, 1, SMALL_TERM_MSG);
+ //     refresh();
+ //   }
+ // }
+
+  do {
+    keyboard_event(getch());
+  } while (!game_won());
+
+  endwin();
+  game_end();
+  printf("You won.\n");
+
+  return (0);
 }
 
-void draw_deck(struct deck *deck) {
-  draw_stack(deck->stock);
-  draw_stack(deck->waste_pile);
-  for (int i = 0; i < FOUNDATION_STACKS_NUMBER; i++) {
-    draw_stack(deck->foundation[i]);
-  }
-  for (int i = 0; i < MANEUVRE_STACKS_NUMBER; i++) {
-    draw_stack(deck->maneuvre[i]);
-  }
+void draw_greeting() {
+  mvprintw(8, 26, "Welcome to tty-solitaire.");
+  mvprintw(10, 21, "Move with the arrow keys or <hjkl>.");
+  mvprintw(11, 18, "Use the space bar to select and place cards.");
+  mvprintw(12, 13, "After selecting a card you can use <m> to select more");
+  mvprintw(13, 13, "and <n> to select less. Press <Shift+M> to select all.");
+  mvprintw(15, 19, "Press the space bar to play or q to quit.");
 }
 
-void draw_cursor(struct cursor *cursor) {
-  if (cursor->marked) {
-    mvwin(cursor->window, cursor->y, cursor->x);
-    waddch(cursor->window, '@');
-  } else {
-    mvwin(cursor->window, cursor->y, cursor->x);
-    waddch(cursor->window, '*');
-  }
-  wrefresh(cursor->window);
+void usage(const char *program_name) {
+  printf("usage: %s [OPTIONS]\n", program_name);
+  printf("  -v, --version              Show version\n");
+  printf("  -h, --help                 Show this message\n");
+  printf("  -p, --passes               Number of passes through the deck  "
+         "(default: 3)\n");
+  printf("      --four-color-deck      Draw unique card suit colors       "
+         "(default: false)\n");
+  printf("      --no-background-color  Don't draw background color        "
+         "(default: false)\n");
 }
 
-void erase_card(struct card *card) {
-  werase(card->frame->window);
-  wbkgd(card->frame->window, WHITE_ON_GREEN);
-  wrefresh(card->frame->window);
-}
-
-void erase_stack(struct stack *stack) {
-  if (maneuvre_stack(stack)) {
-    for (; stack; stack = stack->next) {
-      erase_card(stack->card);
-    }
-  } else {
-    erase_card(stack->card);
-  }
-}
-
-void erase_cursor(struct cursor *cursor) {
-  wdelch(cursor->window);
-  wrefresh(cursor->window);
-}
+void version() { printf("%s\n", VERSION); }
